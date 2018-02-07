888fcb191d367e233f4f414d505fc0cee7ceab52
yang'yang <yangyang@royasoft.com.cn>
2018-02-07 14:15:17 +0800

    文件提交

---
+++ WB/WB/Class/View(视图和视图控制器)/Home/WBHomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WBHomeViewController.swift
+//  WB
+//
+//  Created by yang'yang on 2018/2/7.
+//  Copyright © 2018年 yang'yang. All rights reserved.
+//
+
+import UIKit
+
+class WBHomeViewController: WBBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor=UIColor.red
+    }
+}
--- WB/WB/Class/View(视图和视图控制器)/Main/WBBaseViewController.swift
+++ WB/WB/Class/View(视图和视图控制器)/Main/WBBaseViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WBBaseViewController.swift
 //  WB
 //
 //  Created by yang'yang on 2018/2/7.
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WBBaseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+ 
 
 }
-
