d3fc5107e1d34b43259c0ca40ae735d6ae718f89
yang'yang <yangyang@royasoft.com.cn>
2018-02-07 10:02:58 +0800

    项目创建

---
+++ WB/WB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WB
+//
+//  Created by yang'yang on 2018/2/7.
+//  Copyright © 2018年 yang'yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
